c8d85ff164ad075b457eb3f2d6e9da0ec2636d68
pine <dijsds@163.com>
2026-04-22 14:23:28 +0800

    chore: 删去编译文件

--- robot_yang/build/carlike_robot_description/CMakeFiles/carlike_robot_description_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for carlike_robot_description_uninstall.
--- robot_yang/build/velodyne_simulator/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/zyy/robot_yang/install/velodyne_description/share/velodyne_description/package.sh"
-. "/home/zyy/robot_yang/install/velodyne_gazebo_plugins/share/velodyne_gazebo_plugins/package.sh"
